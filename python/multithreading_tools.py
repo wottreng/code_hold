c4bb75ae8422213b7441f5c3bc2d52d2de3e9ce4
wottreng <89848850+wottreng@users.noreply.github.com>
2022-05-04 18:09:50 -0400

    update multithreading_tools.py

--- python/multithreading_tools.py
+++ python/multithreading_tools.py
@@ -7,15 +7,16 @@ import multiprocessing as mp
 import time
 
 
-def multitasking(target_function, target_function_args:tuple, number_of_tasks:int, verbose=False):
+def multithreading(target_function, target_function_args: list, verbose=False):
     # for tick toc timer:
     if verbose: start_time = time.time()
     #
+    number_of_tasks = len(target_function_args)
     number_of_cores = mp.cpu_count() - 1  # leave one core for main thread
     # build tasking queue ---------
     job_queue = []
     for i in range(number_of_tasks):
-        p = mp.Process(target=target_function, args=target_function_args)
+        p = mp.Process(target=target_function, args=target_function_args[i])
         job_queue.append(p)
     # --- start multiprocessing --------
     active_thread_counter = 0
@@ -39,7 +40,7 @@ def multitasking(target_function, target_function_args:tuple, number_of_tasks:in
     if verbose:
         # calculate how long it took:
         stop_time = time.time()
-        time_diff:float = int(stop_time - start_time)
+        time_diff: float = int(stop_time - start_time)
         #
         time_hours = int(time_diff / 3600)
         time_min = int((time_diff - (time_hours * 3600)) / 60.0)
@@ -49,18 +50,18 @@ def multitasking(target_function, target_function_args:tuple, number_of_tasks:in
 
 
 if __name__ == '__main__':
-
     print("multitasking testing")
 
-    # time intensive testing function
+    # example cpu intensive testing function
     def test_function(num: int, string_var: str):
         a = 100000000
         x = num
         while x < a:
             x += 1
-
         print(f"thread finished, message: {string_var}")
 
-    number_of_function_runs = 20  # how many times does this function need to run
-    args: tuple = (1,"hello")  # what to pass into function
-    multitasking(test_function, args, number_of_function_runs, verbose=True)
+
+    args: list = []
+    for i in range(20):
+        args.append((i, f"hello_#{i}"))  # append a tuple of function arguments
+    multithreading(test_function, args, verbose=True)
